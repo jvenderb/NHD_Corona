27cb7c28f24830507b1689f86ff2de6ec34e3e82
Jeroen Venderbosch <jve@woodwing.com>
2022-01-21 21:19:26 +0100

    Replace  Heerhugowaard en Langedijk  by Dijk en Waard.

--- Application/Config.php
+++ Application/Config.php
@@ -11,7 +11,7 @@ class Config
 {
     public function getCommunityGroups(): array
     {
-        $communities['ALK'] = ['Alkmaar', 'Bergen (NH.)', 'Castricum', 'Heerhugowaard', 'Heiloo', 'Langedijk'];
+        $communities['ALK'] = ['Alkmaar', 'Bergen (NH.)', 'Castricum', 'Dijk en Waard', 'Heerhugowaard', 'Heiloo', 'Langedijk'];
         $communities['KOP'] = ['Den Helder', 'Hollands Kroon', 'Schagen', 'Texel'];
         $communities['WEF'] = [ 'Drechterland', 'Enkhuizen', 'Hoorn', 'Koggenland', 'Medemblik', 'Opmeer', 'Stede Broec'];
         return $communities;
